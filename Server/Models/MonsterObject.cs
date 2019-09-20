b99a4b984789b8d97706817ab5b84875eb87b470
Lilcooldoode <lilcooldoode@gmail.com>
2019-09-20 15:15:25 +0100

    increase mob exp cap

--- Server/Models/MonsterObject.cs
+++ Server/Models/MonsterObject.cs
@@ -2577,7 +2577,7 @@ namespace Server.Models
             if (PetOwner == null && CurrentMap != null)
                 eRate *= 1 + MapExperienceRate / 100M;
             
-            decimal exp = Math.Min(Experience * eRate, 500000000);
+            decimal exp = Math.Min(Experience * eRate, 50000000000);
 
             if (ePlayers.Count == 0)
             {
